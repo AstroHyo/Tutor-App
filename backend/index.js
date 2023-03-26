7a8e175ae2b04ca3c15dfa2fbe9b49b308b09402
Seung Hyo Lee <spinelee2002@gmail.com>
2023-03-26 14:10:54 +0000

    Add web server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,30 @@
 const apiKey = "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";
 const { Configuration, OpenAIApi } = require("openai");
+//express를 불러와서 이걸 app으로 만든다.
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+//CORS 이슈 해결
+// let corsOptions = {
+//   origin: 'https://www.domain.com',
+//   credentials: true
+// }
+app.use(cors());
+
+//POST 요청을 받을 수 있게 해주는 코드 (JSON 데이터 읽기)
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+//POST 요청이 오먼 3000번 포트에 돌려준다.
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+app.listen(3000)
+
+
+
 
 const configuration = new Configuration({
   apiKey: apiKey,
@@ -11,6 +36,13 @@ const situation = ["job interview, and you are a interviewer", "a movie date", "
 async function apiCall(){
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
+
+    //parameter 조정
+    max_tokens: 100,
+    temperature: 0.5,
+    maximum_length: 150,
+
+    //system이랑 user의 초기 prompt 넣어주기
     messages: [
       {role: "system", content: "당신은 세계 최고의 영어 회화 튜터 선생님입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 tutory입니다. 당신의 영어를 매우 잘 가르치고 상대가 영어로 대화를 잘할 수 있도록 도와줍니다. 당신은 다양한 분야에 대해 지식이 풍부하고 상대와의 대화를 잘 이어나갑니다. 당신은 상대와 대화하듯이, 한 번에 모든 대화문을 주진 않고 한 문만 말한 다음 상대의 대답을 기다립니다.        당신은 상대가 문법이 틀리거나 어색한 문장을 사용했을 경우, 이를 자연스러운 문장이 되도록 대화하듯이 고쳐줍니다. 그리고 다시 올바르게 말할 수 있도록 기회를 줍니다. 상대가 올바르게 말했다면 다시 대화를 이어나갑니다."},
       {role: "user", content: "당신은 세계 최고의 영어 회화 튜터 선생님입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 tutory입니다. 당신의 영어를 매우 잘 가르치고 상대가 영어로 대화를 잘할 수 있도록 도와줍니다. 당신은 다양한 분야에 대해 지식이 풍부하고 상대와의 대화를 잘 이어나갑니다. 당신은 상대와 대화하듯이, 한 번에 모든 대화문을 주진 않고 한 문만 말한 다음 상대의 대답을 기다립니다.        당신은 상대가 문법이 틀리거나 어색한 문장을 사용했을 경우, 이를 자연스러운 문장이 되도록 대화하듯이 고쳐줍니다. 그리고 다시 올바르게 말할 수 있도록 기회를 줍니다. 상대가 올바르게 말했다면 다시 대화를 이어나갑니다."},
@@ -18,6 +50,9 @@ async function apiCall(){
       {role: "user", content: "Can we practice a conversation for a" + situation[0] + "?"},
     ],
   });
+
+  //content만 표시
   console.log(completion.data.choices[0].message['content']);
 }
+
 apiCall();
