e164342a5d87c0ce1f8dc19f985728c85d78328b
Seung Hyo Lee <spinelee2002@gmail.com>
2023-03-26 13:23:26 +0000

    Add chatGPT api

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+const apiKey = "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";
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+async function apiCall(){
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{role: "user", content: "Hello world"}],
+  });
+  console.log(completion.data.choices[0].message);
+}
+apiCall();
