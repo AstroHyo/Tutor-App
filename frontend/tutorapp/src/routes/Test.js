11d10c66eaa0c4bfaa48b25464b82e563ad51294
Seung Hyo Lee <spinelee2002@gmail.com>
2023-03-27 07:36:21 +0000

    Build Chat screen with just outline

--- frontend/tutorapp/src/routes/Test.js
+++ frontend/tutorapp/src/routes/Test.js
@@ -1,29 +1,57 @@
 import { useState } from 'react';
-import { Stack, Form, Row, Col } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+//import { Stack, Form, Row, Col } from 'react-bootstrap';
+//import { useNavigate } from 'react-router-dom';
+import './Test.css';
+//https://3000-astrohyo-tutorapp-uife88f4ccs.ws-us92.gitpod.io/tutoringSpeak
 
 function Test() {
-  async function getTutoring() {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+
+  //유저가 채팅 입력 시 setMessageInput
+  const handleInputChange = (event) => {
+    setMessageInput(event.target.value);
+  }
+
+  //Send 버튼 클릭 시, newMessage를 messages 배열에 update
+  const handleSend = async () => {
+    const newMessage = { type: 'user', text: "user: " + messageInput };
+    setMessages([...messages, newMessage]);
+    setMessageInput('');
+
     try {
-        const response = await fetch('https://3000-astrohyo-tutorapp-uife88f4ccs.ws-us92.gitpod.io/tutoringSpeak', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ user : 'John' }) // replace with your desired data
-        });
-        const data = await response.json();
-        console.log(data);
-        return data;
+      const response = await fetch('https://3000-astrohyo-tutorapp-uife88f4ccs.ws-us92.gitpod.io/tutoringSpeak', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          message: messageInput
+        })
+      });
+
+      const data = await response.json();
+
+      const tutorMessage = { type: 'assistant', text: "Tutor: " + data.assistant };
+      setMessages([...messages, tutorMessage]);
     } catch (error) {
-        console.error("somethingwrongbabe"+error);
+      console.error(error);
     }
   }
 
   return (
-    <div className="Test">
-        <h1>테스트</h1>
-        <button onClick={()=>{getTutoring()}}> 요청하기 </button>
+    <div className="chat-container">
+      <div className="chat-box">
+        {messages.map((message, index) => (
+          <div key={index} className={`chat-message ${message.type}`}>
+            <p>{message.text}</p>
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input type="text" placeholder="Type your message here..." value={messageInput} onChange={handleInputChange} />
+        <button onClick={handleSend}>Send</button>
+      </div>
     </div>
   );
 }
