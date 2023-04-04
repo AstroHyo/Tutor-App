66b0285afe21ba4763e1e034125907dc2ce93171
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-04 14:05:23 +0000

    (FE) Add auto scroll and enter key

--- frontend/tutorapp/src/routes/Chatbot.js
+++ frontend/tutorapp/src/routes/Chatbot.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Chatbot.css';
 
 function Chatbot() {
@@ -6,6 +6,18 @@ function Chatbot() {
     { text: "Let's start conversation!", sender: "assistant" }
   ]);
   const [inputValue, setInputValue] = useState("");
+  const chatBoxRef = useRef(null);
+
+  //scroll을 아래로 내려주기 위한 코드
+  useEffect(() => {
+    chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+  }, [messages]);
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      sendMessage();
+    }
+  }
 
   const sendMessage = async () => {
     const response = await fetch('https://b3uiuqz870.execute-api.ap-northeast-2.amazonaws.com/prod/tutoringSpeak', {
@@ -25,7 +37,7 @@ function Chatbot() {
 
   return (
     <div className="chat-container">
-      <div className="chat-box">
+      <div className="chat-box" ref={chatBoxRef}>
         {messages.map((message, index) => (
           <div className={`chat-message ${message.sender}`} key={index}>
             <p>{message.text}</p>
@@ -33,7 +45,7 @@ function Chatbot() {
         ))}
       </div>
       <div className="chat-input">
-        <input type="text" placeholder="Type your message here..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <input type="text" placeholder="Type your message here..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyPress={handleKeyPress} />
         <button onClick={sendMessage}>Send</button>
       </div>
     </div>
