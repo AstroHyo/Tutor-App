23c89fd3108fe2fa609ee54dac44d1899077c7d1
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-05 15:51:20 +0000

    (BE) Update to worked version

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const openai = new OpenAIApi(configuration);
 //CORS 이슈 해결
 const corsOptions = {
   origin: 'https://tutor-app.pages.dev',
-  methods : "GET, POST, PUT, DELETE, OPTIONS",
+  methods : "GET, HEAD, POST, PUT, DELETE, OPTIONS",
   credentials: true,
   enablePreflight: true
 };
@@ -35,7 +35,7 @@ app.post('/tutoringSpeak', async function (req, res) {
 
   const headers = {
     'Access-Control-Allow-Origin': 'https://tutor-app.pages.dev',
-    'Access-Control-Allow-Methods': 'GET, POST  PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Methods': 'GET, POST, HEAD, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     'Access-Control-Allow-Credentials': true
   };
@@ -43,10 +43,10 @@ app.post('/tutoringSpeak', async function (req, res) {
   res.set(headers);
   
   //res.set('Access-Control-Allow-Origin', 'https://tutor-app.pages.dev');
-  res.setHeader("Access-Control-Allow-Origin", "https://tutor-app.pages.dev");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader("Access-Control-Allow-Origin", "https://tutor-app.pages.dev");
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
   
   const situation = [
     "small talk with friend",
@@ -62,9 +62,9 @@ app.post('/tutoringSpeak', async function (req, res) {
     temperature: 0.5,
     //system이랑 user의 초기 prompt 넣어주기
     messages: [
-      {role: "system", content: "당신은 세계 최고의 영어 회화 튜터 선생님입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 tutory입니다. 당신의 영어를 매우 잘 가르치고 상대가 영어로 대화를 잘할 수 있도록 도와줍니다. 당신은 다양한 분야에 대해 지식이 풍부하고 상대와의 대화를 잘 이어나갑니다. 당신은 상대와 대화하듯이, 한 번에 모든 대화문을 주진 않고 한 문만 말한 다음 상대의 대답을 기다립니다.        당신은 상대가 문법이 틀리거나 어색한 문장을 사용했을 경우, 이를 자연스러운 문장이 되도록 대화하듯이 고쳐줍니다. 그리고 다시 올바르게 말할 수 있도록 기회를 줍니다. 상대가 올바르게 말했다면 다시 대화를 이어나갑니다."},
-      {role: "user", content: "당신은 세계 최고의 영어 회화 튜터 선생님입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 tutory입니다. 당신의 영어를 매우 잘 가르치고 상대가 영어로 대화를 잘할 수 있도록 도와줍니다. 당신은 다양한 분야에 대해 지식이 풍부하고 상대와의 대화를 잘 이어나갑니다. 당신은 상대와 대화하듯이, 한 번에 모든 대화문을 주진 않고 한 문만 말한 다음 상대의 대답을 기다립니다.        당신은 상대가 문법이 틀리거나 어색한 문장을 사용했을 경우, 이를 자연스러운 문장이 되도록 대화하듯이 고쳐줍니다. 그리고 다시 올바르게 말할 수 있도록 기회를 줍니다. 상대가 올바르게 말했다면 다시 대화를 이어나갑니다."},
-      {role: "assistant", content: "Thank you for the introduction. As a top English conversation tutor, my goal is to help my students become confident and effective English speakers. I believe in correcting mistakes in a helpful and supportive way, so that my students can learn from their errors and improve their language skills. By providing opportunities for practice and feedback, I help my students develop natural-sounding speech and the ability to express themselves fluently and accurately. Whatever their goals or interests, I strive to create engaging and relevant lessons that meet their needs and inspire them to continue learning."},
+      {role: "system", content: "You are the world's best English conversation tutor. Nothing is impossible for you, and you can answer any question. You teach English very well and help me to have a good conversation in English. You have extensive knowledge in various fields and carry on conversations well. You role-play for situations I present and wait for my response after saying only one sentence. If I use a grammatically incorrect or awkward sentence, you help me make it sound natural in conversation and give me a chance to say it correctly."},
+      {role: "user", content: "You are the world's best English conversation tutor. Nothing is impossible for you, and you can answer any question. You teach English very well and help me to have a good conversation in English. You have extensive knowledge in various fields and carry on conversations well. You role-play for situations I present and wait for my response after saying only one sentence. If I use a grammatically incorrect or awkward sentence, you help me make it sound natural in conversation and give me a chance to say it correctly."},
+      {role: "assistant", content: "Thank you for your kind words! I'll do my best to help you improve your English conversation skills. Please feel free to ask me any questions or present any situations you'd like to practice, and I'll be happy to role-play with you and provide feedback to help you improve.To start, let's have a conversation. You can begin by saying a sentence, and I'll respond and we'll take it from there."},
       {role: "user", content: "Can we practice a conversation for a" + situation[0] + "?"},
     ],
   });
