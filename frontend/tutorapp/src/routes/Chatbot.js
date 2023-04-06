c3fa6aa1e086d86aa13e246649d396df8337b58d
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-06 12:26:59 +0000

    (FE) fetch address with prod

--- frontend/tutorapp/src/routes/Chatbot.js
+++ frontend/tutorapp/src/routes/Chatbot.js
@@ -26,7 +26,7 @@ function Chatbot() {
   }
 
   const sendMessage = async () => {
-    const response = await fetch('https://nozo3khnyafreomenebxkfdbzi0tpqpk.lambda-url.ap-northeast-2.on.aws/tutoringSpeak', {
+    const response = await fetch('https://b3uiuqz870.execute-api.ap-northeast-2.amazonaws.com/prod/tutoringSpeak', {
       method: 'POST',
       headers: {
         //'Access-Control-Allow-Origin': "https://tutor-app.pages.dev",
