bf1ab258ae75c05e73bd74151f4e3526f842bcd8
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-06 10:19:08 +0000

    (FE) Rmove Access-Control-Allow-Origin

--- frontend/tutorapp/src/routes/Chatbot.js
+++ frontend/tutorapp/src/routes/Chatbot.js
@@ -29,7 +29,7 @@ function Chatbot() {
     const response = await fetch('REDACTED_AWS_ENDPOINT', {
       method: 'POST',
       headers: {
-        'Access-Control-Allow-Origin': "https://tutor-app.pages.dev",
+        //'Access-Control-Allow-Origin': "https://tutor-app.pages.dev",
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
@@ -37,6 +37,7 @@ function Chatbot() {
         tutorMessage: tutorMessage,
       })
     });
+
     const data = await response.json();
     setTutorMessage([...tutorMessage, { text: data.assistant, sender: "assistant" }]);
     setInputValue('');
