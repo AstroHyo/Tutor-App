54fe64b88f3ca291de7a10e4a66169282e8f5f16
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-06 13:56:07 +0000

    (FE) Move setUserMessage in to the sendMessage

--- frontend/tutorapp/src/routes/Chatbot.js
+++ frontend/tutorapp/src/routes/Chatbot.js
@@ -20,14 +20,17 @@ function Chatbot() {
   //엔터 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      setUserMessage([...userMessage, { text: inputValue, sender: "user" }]);
       console.log(userMessage);
       console.log(inputValue);
-      //sendMessage();
+      sendMessage();
     }
   }
 
   const sendMessage = async () => {
+    setUserMessage([...userMessage, { text: inputValue, sender: "user" }]);
+    console.log(userMessage);
+    console.log(inputValue);
+
     const response = await fetch('REDACTED_AWS_ENDPOINT', {
       method: 'POST',
       headers: {
