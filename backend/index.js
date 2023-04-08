d0924a4659322947c46127f368d60a386dbd051e
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-08 06:30:32 +0000

    (FE) Add audio record function

--- backend/index.js
+++ backend/index.js
@@ -49,9 +49,7 @@ app.post('/tutoringSpeak', async function (req, res) {
   // res.setHeader('Access-Control-Allow-Credentials', true);
   
   let {userMessage, tutorMessage} = req.body
-  console.log(userMessage);
-  console.log(tutorMessage);
-  
+
   // const situation = [
   //   "small talk with friend",
   //   "job interview, and you are an interviewer",
@@ -60,23 +58,38 @@ app.post('/tutoringSpeak', async function (req, res) {
   // ];
   
   let messages = [
-    {role: "system", content: "You are the world's best English conversation tutor. Nothing is impossible for you, and you can answer any question. You teach English very well and help me to have a good conversation in English. You have extensive knowledge in various fields and carry on conversations well. You role-play for situations I present and wait for my response after saying only one sentence. If I use a grammatically incorrect or awkward sentence, you help me make it sound natural in conversation and give me a chance to say it correctly."},
-    {role: "user", content: "You are the world's best English conversation tutor. Nothing is impossible for you, and you can answer any question. You teach English very well and help me to have a good conversation in English. You have extensive knowledge in various fields and carry on conversations well. You role-play for situations I present and wait for my response after saying only one sentence. If I use a grammatically incorrect or awkward sentence, you help me make it sound natural in conversation and give me a chance to say it correctly."},
+    {role: "system", content: "You are the world's best English conversation tutor. Nothing is impossible for you, and you can answer any question. You teach English very well and help me to have a good conversation in English. You have extensive knowledge in various fields and carry on conversations well. You role-play for situations I present but please don’t give me the full dialogue all at once. Say one sentence and wait for my response. If I use a grammatically incorrect or awkward sentence, you help me make it sound natural in conversation and give me a chance to say it correctly."},
+    {role: "user", content: "You are the world's best English conversation tutor. Nothing is impossible for you, and you can answer any question. You teach English very well and help me to have a good conversation in English. You have extensive knowledge in various fields and carry on conversations well. You role-play for situations I present but please don’t give me the full dialogue all at once. Say one sentence and wait for my response. If I use a grammatically incorrect or awkward sentence, you help me make it sound natural in conversation and give me a chance to say it correctly."},
     {role: "assistant", content: "Thank you for your kind words! I'll do my best to help you improve your English conversation skills. Please feel free to ask me any questions or present any situations you'd like to practice, and I'll be happy to role-play with you and provide feedback to help you improve.To start, let's have a conversation. You can begin by saying a sentence, and I'll respond and we'll take it from there."},
   ];
   
-  while (userMessage.length != 0 || tutorMessage.length != 0) {
-    if (userMessage.length != 0) {
-        messages.push(
-            JSON.parse('{"role": "user", "content": "'+String(userMessage.shift()).replace(/\n/g,"")+'"}')
-        )
-    }
-    if (tutorMessage.length != 0) {
-        messages.push(
-            JSON.parse('{"role": "assistant", "content": "'+String(tutorMessage.shift()).replace(/\n/g,"")+'"}')
-        )
+  let lastRole = "assistant"; // assume assistant sent the last message
+
+  while (userMessage.length > 0 || tutorMessage.length > 0) {
+    if (lastRole === "assistant" && userMessage.length > 0) {
+      messages.push({role: "user", content: userMessage.shift() });
+      lastRole = "user";
+    } else if (lastRole === "user" && tutorMessage.length > 0) {
+      messages.push({role: "assistant", content: tutorMessage.shift() });
+      lastRole = "assistant";
+    } else {
+      break; // both arrays are empty, exit the loop
     }
   }
+
+  
+  // while (userMessage.length != 0 || tutorMessage.length != 0) {
+  //   if (userMessage.length != 0) {
+  //       messages.push(
+  //           JSON.parse('{"role": "user", "content": "'+String(userMessage.shift()).replace(/\n/g,"")+'"}')
+  //       )
+  //   }
+  //   if (tutorMessage.length != 0) {
+  //       messages.push(
+  //           JSON.parse('{"role": "assistant", "content": "'+String(tutorMessage.shift()).replace(/\n/g,"")+'"}')
+  //       )
+  //   }
+  // }
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
