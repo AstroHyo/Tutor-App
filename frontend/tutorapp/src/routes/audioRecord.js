82d11cd041eed12ae1e66c393f29942941f58f59
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-13 13:58:57 +0000

    (FE, record) Edit send button

--- frontend/tutorapp/src/routes/audioRecord.js
+++ frontend/tutorapp/src/routes/audioRecord.js
@@ -47,6 +47,7 @@ const VoiceRecorder = () => {
   const handleSendRecording = async () => {
     try {
       if (audioFile !== null) {
+        console.log(audioFile);
         const response = await axios.post('REDACTED_AWS_ENDPOINT', {
           file: audioFile,
         }, {
@@ -75,7 +76,7 @@ const VoiceRecorder = () => {
         <button className="recordPlayBtn" onClick={handlePlayRecording} disabled={!audioBlob}>
           Play
         </button>
-        <button className="recordSendBtn" onClick={handlePlayRecording} disabled={!audioBlob}>
+        <button className="recordSendBtn" onClick={handleSendRecording} disabled={!audioFile}>
           Play
         </button>
       </div>
