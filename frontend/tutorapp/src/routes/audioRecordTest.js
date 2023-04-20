16ea587d0b062ab21fe3f64cf71501e61394d29b
Seung Hyo Lee <spinelee2002@gmail.com>
2023-04-20 13:47:42 +0000

    (FE, record) Edit recordTest code

--- frontend/tutorapp/src/routes/audioRecordTest.js
+++ frontend/tutorapp/src/routes/audioRecordTest.js
@@ -1,59 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from "react";
+import './audioRecord.css';
+import axios from 'axios';
 
-function AudioRecorder() {
+const AudioRecorder= () => {
   const [recording, setRecording] = useState(false);
-  const [uploading, setUploading] = useState(false);
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [audioBlob, setAudioBlob] = useState(null);
+  const [audioFile, setAudioFile] = useState(null);
+  const audioRef = useRef(null);
+  const [recordText, setRecodeText] = useState('');
 
-  let media = [];
-  let mediaRecorder;
-  const constraints = { audio: true };
-
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.ondataavailable = (e) => {
-        if (e.data.size) {
-          media.push(e.data);
-        }
-      };
+  const handleStartRecording = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream);
       mediaRecorder.start();
       setRecording(true);
-    } catch (error) {
+      setMediaRecorder(mediaRecorder);
+
+      mediaRecorder.addEventListener("dataavailable", (event) => {
+        if (event.data.size > 0) {
+          setAudioBlob(event.data);
+          //녹음본 -> 파일 변환
+          const sound = new File([event.data], "soundBlob.mp3", { lastModified: new Date().getTime(), type: "audio/mpeg" });
+          setAudioFile(sound);
+        }
+      });
+      
+    });
+  };
+
+  const handleStopRecording = () => {
+    if (mediaRecorder !== null) {
+      mediaRecorder.stop();
+      setRecording(false);
+    }
+  };
+
+  const handlePlayRecording = () => {
+    if (audioBlob !== null) {
+      const audioUrl = URL.createObjectURL(audioBlob);
+      audioRef.current.src = audioUrl;
+      audioRef.current.play();
+    }
+  };
+
+  const handleSendRecording = async () => {
+    try {
+      if (audioFile !== null) {
+        console.log(audioFile);
+        const formData = new FormData();
+        formData.append('audio', new Blob(audioFile, { type: 'audio/webm;codecs=opus' }), 'audio.webm');
+        console.log(formData.get('file'));
+        console.log(formData);
+        const response = await fetch('https://jqait94u49.execute-api.ap-northeast-2.amazonaws.com/prod/recordToText', { method: 'POST', body: formData });
+        const data = response.data;
+        setRecodeText(data);
+      } 
+    }
+    catch (error) {
       console.error(error);
     }
   };
 
-  const stopRecording = () => {
-    if (mediaRecorder && mediaRecorder.state !== "inactive") {
-      mediaRecorder.stop();
-      setRecording(false);
-      setUploading(true);
-      console.log(media);
-    }    
-  };
-
-  const upload = async () => {
-    const formData = new FormData();
-    formData.append('audio', new Blob(media, { type: 'audio/webm;codecs=opus' }), 'audio.webm');
-    console.log(formData);
-    await fetch('https://jqait94u49.execute-api.ap-northeast-2.amazonaws.com/prod/recordToText', { method: 'POST', body: formData });
-    setUploading(false);
-  };
-
   return (
     <div>
-      <button onClick={startRecording} disabled={recording || uploading}>
-        {recording ? 'Recording...' : 'Record'}
-      </button>
-      <button onClick={stopRecording} disabled={!recording || uploading}>
-        {uploading ? 'Uploading...' : 'Stop Recording'}
-      </button>
-      <button onClick={upload}>
-        upload
-      </button>
+      <div>
+        {recording ? (
+          <button className="recordStopBtn" onClick={handleStopRecording} disabled={!recording}>■</button>
+        ) : (
+          <button className="recordStartBtn" onClick={handleStartRecording} disabled={recording}>▶</button>
+        )}
+        <button className="recordPlayBtn" onClick={handlePlayRecording} disabled={!audioBlob}>
+          Play
+        </button>
+        <button className="recordSendBtn" onClick={handleSendRecording} disabled={!audioFile}>
+          Send
+        </button>
+      </div>
+      <audio controls ref={audioRef}></audio>
+      <p>{recordText}</p>
     </div>
   );
-}
+};
 
 export default AudioRecorder;
