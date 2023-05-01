41c4956b0141082480d456fa377b0cbbaccf35e8
Seung Hyo Lee <52388644+AstroHyo@users.noreply.github.com>
2023-05-01 12:00:39 +0000

    (FE, TTS) Edit console.log to check voice

--- frontend/tutorapp/src/routes/SpeakChatbot.js
+++ frontend/tutorapp/src/routes/SpeakChatbot.js
@@ -32,7 +32,10 @@ function SpeakChatbot() {
     "Hi, it's nice to meet you. Could you please introduce about the topic of meeting?"
   ];
 
-  const num = [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
+  let [voiceNum, setVoiceNum] = useState([])
+
+  const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+  //[27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
 
   //MIC 설정
   useEffect(() => {
@@ -90,8 +93,9 @@ function SpeakChatbot() {
     // for(var i=0; i<s.length; i++) {
     //   console.log(i + s[i].name + s[i].lang);
     // }
+    console.log(EasySpeech.voices())
     console.log(k)
-    console.log(EasySpeech.voices[k])
+    console.log(EasySpeech.voices()[k])
     await EasySpeech.speak({ 
       text: tutorSpeak,
       voice: EasySpeech.voices()[k],
