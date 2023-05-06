acf80dd1befe44d23b33276f49fd2551af8ff7e4
Seung Hyo Lee <52388644+AstroHyo@users.noreply.github.com>
2023-05-06 13:31:15 +0000

    (FE) Edit App.css for column align

--- frontend/tutorapp/src/routes/ChatbotWithSitu.js
+++ frontend/tutorapp/src/routes/ChatbotWithSitu.js
@@ -248,12 +248,14 @@ function Chatbot() {
       <div>
         {feedback && <div className='Feedback' dangerouslySetInnerHTML={{ __html: feedback.replace(/<h3/g, '<h4 class="feedback-h4"').replace(/<h4/g, '<h4 class="feedback-h4"').replace(/<ul/g, '<ul class="feedback-ul"').replace(/<li/g, '<li class="feedback-li"') }} />}
       </div>
-      <button className="subBtn" style={{marginRight: '30px'}} onClick={() =>{ navigate('/PreRequest')}}>
-        정식 서비스 사전 신청하기✅
-      </button>
-      <button className="subBtn" onClick={() => window.open(`https://tally.so/r/mYPEeN`, "_blank")}>
-        피드백 해주세요🙇🏻‍♂️
-      </button>
+      <div>
+        <button className="subBtn" style={{marginRight: '30px'}} onClick={() =>{ navigate('/PreRequest')}}>
+          정식 서비스 사전 신청하기✅
+        </button>
+        <button className="subBtn" onClick={() => window.open(`https://tally.so/r/mYPEeN`, "_blank")}>
+          피드백 해주세요🙇🏻‍♂️
+        </button>
+      </div>
     </div>
   );
 }
